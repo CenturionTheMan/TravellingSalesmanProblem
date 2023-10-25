176e7c28c9298f694bbf4a32e78a81b8257948fe
Michał <64927330+CenturionTheMan@users.noreply.github.com>
2023-10-25 09:25:44 +0200

    console input update

--- TravellingSalesmanProblemConsole/Program.cs
+++ TravellingSalesmanProblemConsole/Program.cs
@@ -7,31 +7,21 @@ public class Program
 {
     public static void Main()
     {
-        //AdjMatrix matrix = new(15, 1, 100);
-        AdjMatrix? matrix = FilesHandler.LoadAdjMatrixFromFile("G:\\My Drive\\Studia\\Studia_sem_5\\PEA\\TravellingSalesmanProblem\\TestExamples\\matrix_8x8.txt");
-
-        Console.WriteLine(matrix);
-
-        //var matrix = FilesHandler.LoadAdjMatrixFromFile("G:\\My Drive\\Studia\\Studia_sem_5\\PEA\\TravellingSalesmanProblem\\TestExamples\\br17.atsp");
-        DynamicProgramming dynamicProgramming = new();
-        var bestPath = dynamicProgramming.CalculateBestPathCost(matrix);
-
-        Console.WriteLine(bestPath);
-
-        //Test();
-
+        Test();
         Console.WriteLine("DONE");
+        Console.ReadKey();
     }
 
 
     public static void Test()
     {
-        //TimePerformanceTester ptBruteForce = new(new BruteForce());
-        //ptBruteForce.PerformTimeTest("G:\\My Drive\\Studia\\Studia_sem_5\\PEA\\TravellingSalesmanProblem\\TestResults\\");
+        TimePerformanceTester ptBruteForce = new(new BruteForce());
+        ptBruteForce.SetMatrixDistances(10, 1000).SetMatrixSizeForTest(2, 11, 1).SetRepeatAmount(1, 100);
+        ptBruteForce.PerformTimeTest("G:\\My Drive\\Studia\\Studia_sem_5\\PEA\\TravellingSalesmanProblem\\TestResults\\");
 
-        TimePerformanceTester ptDynamicProgramming = new(new DynamicProgramming());
-        ptDynamicProgramming.SetMatrixSizeForTest(10, 20, 1);
-        ptDynamicProgramming.PerformTimeTest("G:\\My Drive\\Studia\\Studia_sem_5\\PEA\\TravellingSalesmanProblem\\TestResults\\");
+        //TimePerformanceTester ptDynamicProgramming = new(new DynamicProgramming());
+        //ptDynamicProgramming.SetMatrixSizeForTest(10, 20, 1);
+        //ptDynamicProgramming.PerformTimeTest("G:\\My Drive\\Studia\\Studia_sem_5\\PEA\\TravellingSalesmanProblem\\TestResults\\");
     }
 }
 
